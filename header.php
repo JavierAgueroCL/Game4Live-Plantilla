bd3727c3b2d874159ed4bd54b8e0c0f84b1ccd51
LectorRSS <the.nightmare.c@gmail.com>
2015-02-28 05:15:09 -0800

    Cambios al Header nuevo

--- header.php
+++ header.php
@@ -17,18 +17,34 @@ wp_head(); //* we need this for plugins and hooks
 <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/jquery.bxslider.css" type="text/css" />
 <script src="<?php bloginfo('template_url'); ?>/assets/js/jquery.bxslider.min.js"></script>
 <link rel='stylesheet' id='theme-mobile-css'  href="<?php bloginfo('template_url'); ?>/style.css" type='text/css' media='all' />
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 <body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage">
-
-    <?php do_action( 'radium_before' ); ?>
+<?php do_action( 'radium_before' ); ?>
 
     <div id="theme-wrapper">
     	<div id="theme-wrapper-inner">
 	    <?php
 	    	do_action( 'radium_after_header' );
 	    	//do_action( 'radium_before_header' );
-	    	do_action( 'radium_header' );
+	    	//do_action( 'radium_header' );
 	    	//do_action( 'radium_after_header' );
 	    ?>
+	 	<nav class="menu-principal" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation">
+	 			<?php
+				/* Main menu */
+				$args = array(
+				    'container' 	=> '',
+					'menu_class' 	=> '',
+				    'fallback_cb' 	=> 'radium_fallback_menu',
+				    'depth' 		=> 5,
+					'theme_location'=> 'primary',
+                    'echo' => 0,
+				    'walker' 		=> new Radium_Walker()
+				);
+
+				echo radium_mega_menu( apply_filters( 'radium_main_menu_args', $args ) );
+			 ?>
+	 	</nav>
 
 		<div id="main-container">
